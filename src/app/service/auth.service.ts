a9189a5c35bcdc7ad8eb74a61ad7cae271a0abf9
Henrique Amaral <hickmanoboy@hotmail.com>
2022-02-19 21:49:55 -0300

    feat: comecando funcionalidade de login

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -8,22 +8,38 @@ import { User } from '../model/User';
 })
 export class AuthService {
 
-  keyId: string ="";
+  keyId: any = "";
 
+  body = {
+    grant_type: "client_credentials",
+    client_id: "g93tbd5lMYY2ikqkypcWTCt4RGidZ3OmjS15w7dLknY5PRxs",
+    client_secret: "pi2kvPGy7QtB31zmqztDPmepgoTJzUYaT43Z1e4mpGu8Sd2A4zBzVImAIYqoIoqQ"
+  };
   constructor(
     private http: HttpClient
   ) { }
 
-  
+  /*login(){
+    let loginResponse = this.http.post('https://openapi.xpi.com.br/oauth2/v1/access-token', this.body, this.token).subscribe((data) => {
+      this.keyId = data?['access_token'];
+    })
+    loginResponse = loginResponse?['access_token']
+    this.keyId = loginResponse
+  }*/
 
   token = {
-    headers: new HttpHeaders().set('Authorization', this.keyId)
+    headers: new HttpHeaders().set('Authorization', this.keyId).append('Content-Type', 'application/json')
   }
-
   getAllUser(): Observable<User>{
-    return this.http.get<User>('https://openapi.xpi.com.br/openbanking/users', this.token);
+    const param = {
+      params: {limit: '100'}
+    }
+    const options = {
+      ...this.token,
+      ...param
+    }
+    return this.http.get<User>('https://openapi.xpi.com.br/openbanking/users', options);
   }
-  
 
 
 
