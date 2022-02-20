ee402d4b4455b4ad3e9ce462132b52dca931e68b
Adan Vinicius <viniciusadan123@gmail.com>
2022-02-19 21:21:06 -0300

    @ad4nv1[Task: Created service and components]

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../model/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  keyId: string ="";
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', this.keyId)
+  }
+
+  getAllUser(): Observable<User>{
+    return this.http.get<User>('https://openapi.xpi.com.br/openbanking/users', this.token);
+  }
+  
+
+
+
 }
